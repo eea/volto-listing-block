0cc27020c0fe6c4b354d12b1bfdd16a29858809c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-08-12 08:47:20 +0300

    Refactor Card to split subcomponents

--- src/UniversalCard.jsx
+++ src/UniversalCard.jsx
@@ -6,57 +6,75 @@ import PreviewImage from './PreviewImage';
 import { truncate } from 'lodash';
 import cx from 'classnames';
 
-const BasicCard = ({
-  item,
-  isEditMode,
-  hasDate,
-  hasDescription,
-  maxDescription,
-  styles,
-}) => {
-  const { title, description, EffectiveDate } = item;
+const CardMetaDate = (props) => {
+  const { item, hasDate } = props;
+  const { EffectiveDate } = item;
   const locale = config.settings.dateLocale || 'en-gb';
+
+  return hasDate && EffectiveDate !== 'None' ? (
+    <UiCard.Meta>
+      {formatDate({
+        date: EffectiveDate,
+        format: {
+          year: 'numeric',
+          month: 'short',
+          day: '2-digit',
+        },
+        locale: locale,
+      })}
+    </UiCard.Meta>
+  ) : null;
+};
+
+const CardTitle = (props) => {
+  const { item, isEditMode } = props;
+  const { title } = item;
+
+  return title ? (
+    <UiCard.Header>
+      <ConditionalLink
+        className="header-link"
+        item={item}
+        condition={!isEditMode}
+      >
+        {title}
+      </ConditionalLink>
+    </UiCard.Header>
+  ) : null;
+};
+
+const CardDescription = (props) => {
+  const { item, hasDescription, maxDescription } = props;
+  const { description } = item;
+
+  return hasDescription && description ? (
+    <UiCard.Description>
+      {maxDescription
+        ? truncate(description, {
+            length: maxDescription,
+            separator: ' ',
+          })
+        : description}
+    </UiCard.Description>
+  ) : null;
+};
+
+const CardImage = ({ item, isEditMode }) => (
+  <ConditionalLink className="image" item={item} condition={!isEditMode}>
+    <PreviewImage item={item} alt={item.title} />
+  </ConditionalLink>
+);
+
+const BasicCard = (props) => {
+  const { styles } = props;
 
   return (
     <UiCard fluid={true} className={cx(styles?.theme)}>
-      <ConditionalLink className="image" item={item} condition={!isEditMode}>
-        <PreviewImage item={item} alt={item.title} />
-      </ConditionalLink>
+      <CardImage {...props} />
       <UiCard.Content>
-        {hasDate && EffectiveDate !== 'None' && (
-          <UiCard.Meta>
-            {formatDate({
-              date: EffectiveDate,
-              format: {
-                year: 'numeric',
-                month: 'short',
-                day: '2-digit',
-              },
-              locale: locale,
-            })}
-          </UiCard.Meta>
-        )}
-        {title && (
-          <UiCard.Header>
-            <ConditionalLink
-              className="header-link"
-              item={item}
-              condition={!isEditMode}
-            >
-              {title}
-            </ConditionalLink>
-          </UiCard.Header>
-        )}
-        {hasDescription && description && (
-          <UiCard.Description>
-            {maxDescription
-              ? truncate(description, {
-                  length: maxDescription,
-                  separator: ' ',
-                })
-              : description}
-          </UiCard.Description>
-        )}
+        <CardMetaDate {...props} />
+        <CardTitle {...props} />
+        <CardDescription {...props} />
       </UiCard.Content>
     </UiCard>
   );
