681324ecec2c7704a87a94420d5b5f051a5ceb4b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-09-14 12:58:25 +0300

    Remove apply form defaults, there's already one in Volto

--- src/schema-utils.js
+++ src/schema-utils.js
@@ -1,4 +1,4 @@
-import { cloneDeep, isEqual } from 'lodash';
+import { cloneDeep } from 'lodash';
 import config from '@plone/volto/registry';
 
 const addTypeSelect = ({ intl, schema, extensionName, messages }) => {
@@ -36,30 +36,6 @@ export const enhanceSchema = ({ extensionName, messages }) => ({
     : cloneDeep(originalSchema);
 
   return addTypeSelect({ schema, intl, extensionName, messages });
-};
-
-export const applySchemaDefaults = (schema, props) => {
-  const { data } = props;
-  const objectSchema = typeof schema === 'function' ? schema(props) : schema;
-  const initialData = {
-    ...Object.keys(objectSchema.properties).reduce(
-      (accumulator, currentField) =>
-        objectSchema.properties[currentField].default
-          ? {
-              ...accumulator,
-              [currentField]: objectSchema.properties[currentField].default,
-            }
-          : accumulator,
-      {},
-    ),
-    ...data,
-  };
-
-  Object.keys(initialData).forEach((k) => {
-    if (!isEqual(initialData[k], data?.[k])) {
-      // onChangeField(k, initialData[k]);
-    }
-  });
 };
 
 export const getVoltoStyles = (props) => {
