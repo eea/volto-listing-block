72e1e9e5c83bcbf81c2c18e2c0181064d610b236
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-11-10 14:16:45 +0200

    put a space

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,5 +1,6 @@
 import { getFieldURL } from './helpers';
 import { flattenToAppURL } from '@plone/volto/helpers';
+
 it('test getFieldUrl', () => {
   expect(getFieldURL({ value: 'some-link', '@type': 'URL' })).toEqual(
     'some-link',
