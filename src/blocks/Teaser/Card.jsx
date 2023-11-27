da1d55a314117cfc4cb80ba63bcf880219995607
Dobricean Ioan Dorian <dobriceanionut1408@gmail.com>
2023-11-27 14:00:12 +0200

    use image widget for image override

--- src/blocks/Teaser/Card.jsx
+++ src/blocks/Teaser/Card.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { omit } from 'lodash';
 import UniversalCard from '@eeacms/volto-listing-block/components/UniversalCard/UniversalCard';
 import ImageWidget from './ImageWidget';
+import { isInternalURL } from '@plone/volto/helpers';
 import '@eeacms/volto-listing-block/less/teaser-cards.less';
 
 const TeaserCardTemplate = (props) => {
@@ -15,12 +16,27 @@ const TeaserCardTemplate = (props) => {
     ...rest
   } = props;
   const item = data.href?.[0];
-
-  return item ? (
+  return item || data.preview_image ? (
     <UniversalCard
       isEditMode={isEditMode}
       {...rest}
-      {...data}
+      {...{
+        ...data,
+        preview_image_url:
+          typeof data.preview_image === 'string' &&
+          !isInternalURL(data.preview_image)
+            ? data.preview_image
+            : '',
+        preview_image: [
+          typeof data.preview_image === 'string'
+            ? {
+                '@id': data.preview_image,
+                url: data.preview_image,
+                title: data.preview_image,
+              }
+            : data.preview_image,
+        ],
+      }}
       item={{ ...(item || {}), ...omit(data, ['@type']) }}
       itemModel={data.itemModel || {}}
     />
@@ -31,8 +47,7 @@ const TeaserCardTemplate = (props) => {
       onChange={(e, id) => {
         onChangeBlock(block, {
           ...data,
-          preview_image_url: typeof id === 'string' ? id : '',
-          href: [typeof id === 'string' ? { id: id, url: id, title: id } : id],
+          preview_image: typeof id === 'string' ? id : id?.['@id'],
         });
       }}
     />
