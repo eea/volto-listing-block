2a880167e1166a826d58e5962a136808f9e01f22
David Ichim <ichim.david@gmail.com>
2025-04-09 14:03:47 +0300

    feat(visualization-card): display EEA target taxonomy
    This commit introduces the display of the EEA target taxonomy within the VisualizationCard component.
    
    - Added a `CardEEATarget` component to render the EEA target taxonomy, including styling and capitalization.
    - Modified the `VisualizationCard` component to include the `CardEEATarget` component.
    - Updated the `visualization-cards.less` file to include styling for the EEA target taxonomy display.
    - Added PropTypes for the new component and updated existing ones.

--- src/blocks/Listing/item-templates/VisualizationCard.jsx
+++ src/blocks/Listing/item-templates/VisualizationCard.jsx
@@ -1,4 +1,6 @@
 import cx from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Card as UiCard } from 'semantic-ui-react';
 
@@ -22,15 +24,41 @@ const getStyles = (props) => {
   return res;
 };
 
+const capitalize_eea_target = (str) => {
+  if (!str) return '';
+  const txt = str.replace('-', ' ');
+  return txt.charAt(0).toUpperCase() + txt.slice(1);
+};
+
+const CardEEATarget = ({ item }) => {
+  const eea_target = item?.['taxonomy_eea_target']?.[0];
+  if (!eea_target) {
+    return null;
+  }
+  return (
+    <div className="taxonomy_eea_target_wrapper">
+      <div className={`metadata taxonomy_eea_target ${eea_target}`}>&nbsp;</div>
+      {capitalize_eea_target(eea_target)}
+    </div>
+  );
+};
+
+CardEEATarget.propTypes = {
+  item: PropTypes.shape({
+    taxonomy_eea_target: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
 const VisualizationCard = (props) => {
-  const { className } = props;
+  const { className, item } = props;
   const imagePosition = props.imagePosition;
   const preview_image_url =
-    props.item['@id'] + '/@@plotly_preview.svg/soer_miniature';
+    item['@id'] + '/@@plotly_preview.svg/soer_miniature';
   return (
     <UiCard fluid={true} className={cx('u-card', getStyles(props), className)}>
       <UiCard.Content>
         <CardTitle {...props} />
+        <CardEEATarget item={item} />
         <CardDescription {...props} />
         <CardImage {...props} preview_image_url={preview_image_url} />
         <CardMeta {...props} />
@@ -40,6 +68,21 @@ const VisualizationCard = (props) => {
       <CardExtra {...props} />
     </UiCard>
   );
+};
+
+VisualizationCard.propTypes = {
+  item: PropTypes.shape({
+    '@id': PropTypes.string.isRequired,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    preview_image_url: PropTypes.string,
+    meta: PropTypes.string,
+    extra: PropTypes.any,
+    imagePosition: PropTypes.string,
+    taxonomy_eea_target: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+  className: PropTypes.string,
+  imagePosition: PropTypes.string,
 };
 
 export default VisualizationCard;
