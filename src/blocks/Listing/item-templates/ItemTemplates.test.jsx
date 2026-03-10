4da51b78f0e1868d22d5f4e08f71c893dbc655eb
Alin Voinea <contact@avoinea.com>
2026-03-10 23:53:23 +0200

    tests: Fix jest-dom import

--- src/blocks/Listing/item-templates/ItemTemplates.test.jsx
+++ src/blocks/Listing/item-templates/ItemTemplates.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { DefaultItemLayout } from './ItemTemplates';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 jest.mock('@plone/volto/components', () => ({
   ConditionalLink: () => <div>ConditionalLink</div>,
--- src/components/UniversalCard/fragments/CardExtra.test.jsx
+++ src/components/UniversalCard/fragments/CardExtra.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import CardExtra from './CardExtra';
 
 // Mock the dependencies
